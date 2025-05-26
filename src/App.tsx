5505c8d263528e879328e6732200fc6ea16395c7
Rawan Ahmed <RawanSadek267@gmail.com>
2025-05-26 22:31:26 +0300

    routes and AuthLayout handled

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import './App.css'
-import { Box, Grid } from '@mui/material';
+import AuthLayout from './Components/AuthLayout/AuthLayout';
+import NotFound from './Components/NotFound/NotFound';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import MasterLayout from './Components/MasterLayout/MasterLayout';
 
 function App() {
-    
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <AuthLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Login /> },
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> }
+
+      ]
+    },
+    {
+      path: 'dashboard',
+      element: <MasterLayout />,
+      errorElement: <NotFound />
+    }
+  ])
+
   return (
     <>
-    app
+      <RouterProvider router={routes}></RouterProvider>
     </>
   );
 }
--- src/Components/AuthLayout/AuthLayout.tsx
+++ src/Components/AuthLayout/AuthLayout.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import authImg from '../../assets/bookstorimg.jpg'
 
 export default function AuthLayout() {
   return (
-    <div>
-      Auth
+    <div className='row justify-content-center'>
+        <div className="col-12 col-md-6 order-sm-2 order-md-0">
+            <img src={authImg} alt="book store image" className='w-100'/>
+        </div>
+        <div className="col-12 col-md-6 order-sm-1 order-md-2">
+            auth
+        </div>
     </div>
   )
 }
