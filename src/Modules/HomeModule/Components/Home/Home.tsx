1954679b267f7a5bdb3f430f379e4db2d7ba246d
Rawan Ahmed <RawanSadek267@gmail.com>
2025-06-04 01:39:20 +0300

    updated

--- src/Modules/HomeModule/Components/Home/Home.tsx
+++ src/Modules/HomeModule/Components/Home/Home.tsx
@@ -1,7 +1,11 @@
 // import "swiper/css/bundle";
 
 import { Box } from "@mui/material";
-import Item from "@mui/material/Grid";
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import '../node_modules/swiper/swiper-bundle.min.css'
+import '../../../../../node_modules/swiper/swiper-bundle.min.css'
+
+// import { Navigation, Pagination } from 'swiper';
 import { useNavigate } from "react-router-dom";
 import NavBar from "../../../Shared/Components/NavBar/NavBar";
 
@@ -10,11 +14,27 @@ export default function Login() {
   return (
     <>
     <div><NavBar /></div>
-    <Box>
-      <Item >
-        home
-      </Item>
-      </Box> 
+    <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        // modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </>
   )
 }
